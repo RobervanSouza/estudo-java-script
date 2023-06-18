c0cae3e8d0108374f5a73351f8d41f0a76736ca5
Robervan Souza <103292038+RobervanSouza@users.noreply.github.com>
2023-06-18 18:04:06 -0300

    concluido o addEventList

--- 3 AddEventListen/index.js
+++ 3 AddEventListen/index.js
@@ -8,9 +8,39 @@ function changeColor() {
     const colors = [ 'red', 'blue', 'green', 'yellow', 'purple' ];
     const randomColor = colors[ Math.floor(Math.random() * colors.length) ];
     alterarCor.style.color = randomColor;
+
 }
 
 alterarCor.addEventListener('click', changeColor);
 
 
 // 3 ####### nivel dificil #############
+const clickButton = document.getElementById('clickButton');
+const clickCount = document.getElementById('clickCount');
+let count = 0;
+
+function incrementCount() {
+    count++;
+    clickCount.textContent = `Total de cliques: ${count}`;
+}
+clickButton.addEventListener('click', incrementCount);
+
+// 3 ############## Super Dificil #############
+
+const squares = document.querySelectorAll('#squareTable td');
+
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[ Math.floor(Math.random() * 16) ];
+    }
+    return color;
+}
+
+squares.forEach(square => {
+    square.addEventListener('click', function () {
+        const color = getRandomColor();
+        this.style.backgroundColor = color;
+    });
+});
--- novoitem/index.js
+++ novoitem/index.js
@@ -1,10 +1,8 @@
 
 
-//3 ############## Nivel Intermediario #############
+//3 >>>>>>>>>> Nivel Intermediario <<<<<<<<<<<
 
 
-// 3 ####### Nivel Dificil #############
+// 3 >>>>>>>>>> Nivel Dificil <<<<<<<<<<<
 
-
-
-// 3 ####### Nivel  Super Dificil #############
+// 3 >>>>>>>>>> Nivel  Super Dificil <<<<<<<<<<<
